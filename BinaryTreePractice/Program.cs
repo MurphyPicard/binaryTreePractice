6d8f34704fa17c96f437f54b3918c5adf8e4a487
Ara <ayaghsizian@gmail.com>
2018-01-22 17:40:03 -0500

    made class Node and making class BinarySearchTree

--- BinaryTreePractice/Program.cs
+++ BinaryTreePractice/Program.cs
@@ -6,6 +6,40 @@ using System.Threading.Tasks;
 
 namespace BinaryTreePractice
 {
+
+    public class Node
+    {
+        public int Data;
+        public Node LeftChild;
+        public Node RightChild;
+    }
+    public class BinarySearchTree
+    {
+        public Node root;
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+        public void add(int data)
+        {
+            Node newNode = new Node();
+            newNode.Data = data;
+            if (root = null)
+            {
+                root = newNode;
+            }
+            else
+            {
+                // pause for now
+            }
+        }
+    }
+
+
+
+
+
+
     class Program
     {
         static void Main(string[] args)
@@ -13,4 +47,6 @@ namespace BinaryTreePractice
             // initial commit
         }
     }
+
+    
 }
