430c4ec878272acc1835b146035c21d3487a9add
Ara <ayaghsizian@gmail.com>
2018-01-27 11:03:10 -0500

    refactored classes into their own files

---
+++ BinaryTreePractice/BinarySearchTree.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace BinaryTreePractice
+{
+    public class BinarySearchTree
+    {
+        public Node root;
+        public BinarySearchTree()
+        {
+            //making this a new tree
+            root = null;
+        }
+        public void add(int data)
+        {
+            // making a new node
+            Node newNode = new Node();
+            newNode.Data = data;
+            Console.WriteLine("Dropping plinko chip number " + newNode.Data + "... ");
+
+            if (root == null)
+            {
+                // make the first number the root
+                root = newNode;
+            }
+            else
+            {
+
+                Node current = root;
+                Node parent;
+                while (true)
+                {
+                    // sets the current root to be the parent
+                    parent = current;
+                    if ( data < current.Data)
+                    {
+                        current = current.LeftChild;
+                        
+                        // if no leftchild, one will be created here
+                        if (current == null)
+                        {
+                            parent.LeftChild = newNode;
+                            Console.WriteLine("It worked!!! (whew)");
+                            Console.WriteLine(parent.LeftChild.Data + " is the LeftChild of " + parent.Data);
+                            // if we create a left child we can break out of the loop
+                            break;
+                        }
+                    }
+                    else
+                    {
+
+                        current = current.RightChild;
+
+                        // if no rightchild, one will be created here
+                        if (current == null)
+                        {
+                            parent.RightChild = newNode;
+                            Console.WriteLine("It worked!!! (whew)");
+                            Console.WriteLine( parent.RightChild.Data + " is the RightChild of " + parent.Data);
+                            // if we create a right child we can break out of the loop
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    
+}
---
+++ BinaryTreePractice/Node.cs
@@ -0,0 +1,12 @@
+﻿namespace BinaryTreePractice
+{
+    public class Node
+    {
+        // each node is data with a leftchild and rightchild
+        public int Data;
+        public Node LeftChild;
+        public Node RightChild;
+    }
+
+    
+}
