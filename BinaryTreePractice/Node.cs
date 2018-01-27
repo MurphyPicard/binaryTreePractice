652a9938d516201e361014f9d61268e108240b89
Ara <ayaghsizian@gmail.com>
2018-01-27 12:19:01 -0500

    edited class Node

--- BinaryTreePractice/Node.cs
+++ BinaryTreePractice/Node.cs
@@ -2,7 +2,7 @@
 {
     public class Node
     {
-        // each node is data with a leftchild and rightchild
+        // All binary trees have a left and right child, the datatype can be anything.
         public int Data;
         public Node LeftChild;
         public Node RightChild;
